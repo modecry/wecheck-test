b64add2e1ec0f6fe854939058dbb3df74f4ecd44
Alexandr Kudryashov <a.kudryashov@lipt-soft.ru>
2022-05-15 23:52:18 +0300

    fix problems

--- src/modules/Product/view/ProductList/constants/SORT_PATHS.ts
+++ src/modules/Product/view/ProductList/constants/SORT_PATHS.ts
@@ -1,9 +1,9 @@
 export const SORT_PATHS = {
-    ['name']: 'Название товара',
-    ['inner_product_id']: 'Артикул',
-    ['brand.name']: 'Бренд',
-    ['seller.name']: 'Продавец',
-    ['last_price']: 'Цена',
-    ['sold_money']: 'Заказы Р',
-    ['sold_number']: 'Заказы шт',
+    name: 'Название товара',
+    inner_product_id: 'Артикул',
+    'brand.name': 'Бренд',
+    'seller.name': 'Продавец',
+    last_price: 'Цена',
+    sold_money: 'Заказы Р',
+    sold_number: 'Заказы шт',
 }
